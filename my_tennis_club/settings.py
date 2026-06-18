186bf05a1b766bcaa63c260431f6678feae1bd4f
Sharmin <shar28min@gmail.com>
2026-06-18 20:23:18 +0600

    upgrate setting file

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
     )
 }
 
@@ -120,4 +121,4 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
