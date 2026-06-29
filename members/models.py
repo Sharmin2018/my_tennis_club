4ffd1c61dd43b4e1d48fff6a253c7a07bc7782f7
Sharmin <shar28min@gmail.com>
2026-06-29 16:11:16 +0600

    upgrade project

--- members/models.py
+++ members/models.py
@@ -1,7 +1,10 @@
+
 from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
+    email = models.EmailField(default="")
+    age = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
--- members/views.py
+++ members/views.py
@@ -1,11 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Student
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-def home(request):
+
+
+@login_required(login_url='/login/')
+
+def student_list(request):
+    print("USER:", request.user)
+    query = request.GET.get('q')
+
+    if query:
+        students = Student.objects.filter(
+            Q(name__icontains=query) |
+            Q(email__icontains=query)
+        )
+    else:
+        students = Student.objects.all()
+
+    return render(request, 'list.html', {'students': students})
+
+@login_required(login_url='/login/')
+def add_student(request):
     if request.method == "POST":
-        name = request.POST.get("username")
-        Student.objects.create(name=name)
+        name = request.POST['name']
+        email = request.POST['email']
+        age = request.POST['age']
+        Student.objects.create(name=name, email=email, age=age)
+        return redirect('list')
+    return render(request, 'add.html')
 
-    students = Student.objects.all()
+@login_required(login_url='/login/')
+def edit_student(request, id):
+    student = get_object_or_404(Student, id=id)
+    if request.method == "POST":
+        student.name = request.POST['name']
+        student.email = request.POST['email']
+        student.age = request.POST['age']
+        student.save()
+        return redirect('list')
+    return render(request, 'edit.html', {'student': student})
 
-    return render(request, "home.html", {"students": students})
+@login_required(login_url='/login/')
+def delete_student(request, id):
+    student = get_object_or_404(Student, id=id)
+    student.delete()
+    return redirect('list')
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists!")
+            return redirect('register')
+
+        User.objects.create_user(
+            username=username,
+            password=password
+        )
+
+        messages.success(request, "Account created successfully!")
+        return redirect('login')
+
+    return render(request, 'register.html')
